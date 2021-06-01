5a5bb638a8876f854fe9bf52a304b2c5ddec83b5
guangda.dong <guangda.dong@streamcomputing.com>
2021-06-01 10:02:40 +0800

    [vector] fixed vmaskInsn split cnt

--- src/main/scala/exu/decode.scala
+++ src/main/scala/exu/decode.scala
@@ -918,10 +918,10 @@ class DecodeUnit(implicit p: Parameters) extends BoomModule
 
     // vmasklogic 
     val vmlogic_insn = cs.uopc.isOneOf(uopVMAND, uopVMNAND, uopVMANDNOT, uopVMXOR, uopVMOR, uopVMNOR, uopVMORNOT, uopVMXNOR)
-    // sew = 3.U => 64bit element
-    val vmlogic_split_ecnt = vLen.U >> (6.U)
-    //val eLen_ecnt = eLen.U >> (vsew+3.U)
-    val vmlogic_tolal_ecnt = vlmax >> (3.U - vsew)
+    // 3.U => Byte index, vsew => element num, 3.U => 64bit element, vlmul_sign => vmul
+    val vmlogic_split_ecnt = vLen.U >> (3.U + vsew + 3.U - io.csr_vconfig.vtype.vlmul_sign)
+    // 3.U => 64bit element, vsew => element num
+    val vmlogic_tolal_ecnt = vlmax >> (3.U + vsew)
 
 
     //val eLen_ecnt = eLen.U >> (vsew+3.U)
