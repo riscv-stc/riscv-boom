d28fcc838e6a42672906aa884798ca14e37705d3
guangda.dong <guangda.dong@streamcomputing.com>
2021-06-08 09:21:35 +0800

    [vector] fixed vmlogic mask value

--- src/main/scala/exu/execution-units/functional-unit.scala
+++ src/main/scala/exu/execution-units/functional-unit.scala
@@ -507,7 +507,7 @@ class ALUUnit(
   val vmlogic_vl = (vl(0) || vl(1) || vl(2) || vl(3) || vl(4) || vl(5)) +& (vl>>(byteWidth +& vsew64bit))
   val is_vmlogic_last_ecnt = vstart === (vmlogic_vl-1.U)
 
-  val vmlogic_mask = boom.util.MaskGen(0.U, vmlogic_vl(5,0), 64)
+  val vmlogic_mask = boom.util.MaskGen(0.U, vl(5,0), 64)
   val vmlogic_alu_result = Mux(io.req.bits.uop.uopc.isOneOf(uopVMNAND, uopVMNOR, uopVMXNOR), ~alu.io.out, alu.io.out)
   val vmlogic_last_result = (vmlogic_alu_result & vmlogic_mask) | (io.req.bits.rs3_data & (~vmlogic_mask))
   val vmlogic_result = Mux(is_vmlogic_last_ecnt, vmlogic_last_result, vmlogic_alu_result)
